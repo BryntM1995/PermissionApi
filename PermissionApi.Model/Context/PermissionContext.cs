970c0f7fdb77fda6803898d112b67f7d9526115f
BryntM1995 <Bryant-m@hotmail.com>
2024-04-05 17:12:54 -0400

    Migration set

---
+++ PermissionApi.Model/Context/PermissionContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PermissionManagement.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PermissionManagement.Model.Context
+{
+    public class PermissionContext : DbContext
+    {
+        public DbSet<Permission> Permission { get; set; }
+        public DbSet<PermissionType> PermissionType { get; set; }
+        public PermissionContext(DbContextOptions options) : base(options)
+        {
+        }
+        public PermissionContext()
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Permission>().HasQueryFilter(x => x.IsDeleted == false);
+            modelBuilder.Entity<PermissionType>().HasQueryFilter(x => x.IsDeleted == false);
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+        
+        public override int SaveChanges()
+        {
+            // Get the entries that are auditable
+            var entries = ChangeTracker.Entries<IAuditEntity>();
+
+            if (entries == null)
+                return base.SaveChanges();
+
+            var userId = Guid.NewGuid().ToString();
+            var currentDate = DateTime.Now;
+
+            var validStates = new HashSet<EntityState> { EntityState.Added, EntityState.Modified, EntityState.Deleted };
+            var entriesFiltered = entries.Where(x => validStates.Contains(x.State));
+
+            foreach (var entry in entriesFiltered)
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = currentDate;
+                        entry.Entity.CreatedBy = userId;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.UpdatedDate = currentDate;
+                        entry.Entity.UpdatedBy = userId;
+                        break;
+                    case EntityState.Deleted:
+                        ((ISoftDeleteEntity)entry.Entity).IsDeleted = true;
+                        entry.State = EntityState.Modified;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
---
+++ PermissionApi.Model/Entities/Permission.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PermissionManagement.Model.Entities
+{
+    public class Permission : BaseEntity, ISoftDeleteEntity, IAuditEntity
+    {
+        public bool IsDeleted { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        [Required]
+        public string EmployeeFirstName { get; set; }
+        [Required]
+        public string EmployeeLastName { get; set; }
+        public virtual PermissionType PermissionType { get; set; }
+        [Required]
+        [ForeignKey("PermissionType")]
+        public int PermissionTypeId { get; set; }
+        [Required]
+        public DateTime PermissionDate { get; set; }
+        
+    }
+}
